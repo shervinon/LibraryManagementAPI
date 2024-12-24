5575acd1fe0dc23f96b10c67e13cde95727b3c63
Shervin Ahangari <125131336+shervinon@users.noreply.github.com>
2024-12-24 20:11:37 +0330

    Update Database-Setup.sql

--- Database-Setup.sql
+++ Database-Setup.sql
@@ -1,15 +1,31 @@
 create database library;
 use library;
+create table users
+(
+	id int auto_increment primary key,
+    name varchar(255) not null
+);
 create table books 
 (
 	id int auto_increment primary key,
     title varchar(255) not null,
     author varchar(255) not null,
     is_borrowed boolean default false,
-    borrowed_by int default null
+    borrowed_by int default null,
+    borrowed_start_date DATE NULL,
+    borrowed_end_date DATE NULL,
+    foreign key (borrowed_by) references users(id)
 );
-create table users
+create table categories 
 (
-	id int auto_increment primary key,
-    name varchar(255) not null
+    id int auto_increment primary key,
+    name varchar(255) not null unique
+);
+create table book_categories 
+(
+    book_id int,
+    category_id int,
+    primary key (book_id, category_id),
+    foreign key (book_id) references books(id) on delete cascade,
+    foreign key (category_id) references categories(id) on delete cascade
 );
