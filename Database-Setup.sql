7447a65c9ce1092c42b775cc673351eb664bf790
Shervin Ahangari <125131336+shervinon@users.noreply.github.com>
2025-02-06 19:37:56 +0330

    Update Database-Setup.sql

--- Database-Setup.sql
+++ Database-Setup.sql
@@ -3,13 +3,15 @@ use library;
 create table users
 (
 	id int auto_increment primary key,
-    name varchar(255) not null
+    name varchar(200) not null,
+    username varchar(50) not null unique,
+    password varchar(255) not null
 );
 create table books 
 (
 	id int auto_increment primary key,
-    title varchar(255) not null,
-    author varchar(255) not null,
+    title varchar(200) not null,
+    author varchar(200) not null,
     is_borrowed boolean default false,
     borrowed_by int default null,
     borrowed_start_date DATE NULL,
@@ -19,7 +21,7 @@ create table books
 create table categories 
 (
     id int auto_increment primary key,
-    name varchar(255) not null unique
+    name varchar(200) not null unique
 );
 create table book_categories 
 (
